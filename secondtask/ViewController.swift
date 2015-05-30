f2a58607cfd500198d9ea0b5181c744de47e02fd
JValderramaN <josevalderrama18@gmail.com>
2015-05-30 10:55:30 -0430

    data

--- secondtask/ViewController.swift
+++ secondtask/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
 
     @IBOutlet weak var tfSearch: UITextField!
     @IBOutlet weak var btSearch: UIButton!
@@ -18,11 +18,46 @@ class ViewController: UIViewController {
     @IBOutlet weak var btPopular3: UIImageView!
     @IBOutlet weak var btPopular4: UIImageView!
     
-    var data = [["id":1,"name":"Accounting"],["id":2,"name":"Art & Design"],["id":3,"name":"Biology"],["id":4,"name":"Business & Finance"],["id":5,"name":"Calculus"],["id":6,"name":"Chemistry"],["id":7,"name":"Economics"],["id":8,"name":"Engineering"],["id":9,"name":"English"],["id":10,"name":"Environmental Science"],["id":10,"name":"Foreign Languages"],["id":11,"name":"Geology"],["id":12,"name":"Health & Medical"],["id":13,"name":"History"],["id":14,"name":"Law"],["id":15,"name":"Marketing"],["id":16,"name":"Mathematics"],["id":17,"name":"Philosophy"],["id":18,"name":"Physics"],["id":19,"name":"Political Science"],["id":20,"name":"Computer Science"],["id":21,"name":"Psychology"],["id":22,"name":"Science"],["id":23,"name":"Social Science"],["id":24,"name":"Sociology"],["id":25,"name":"Statistics"],["id":26,"name":"Writing"],["id":27,"name":"Other"],["id":28,"name":"Algebra"],["id":29,"name":"Programming"],["id":30,"name":"Communications"],["id":31,"name":"Film"],["id":32,"name":"Management"],["id":33,"name":"SAT"]]
+    var pickerData = [
+        ["id":1,"name":"Accounting"],
+        ["id":2,"name":"Art & Design"],
+        ["id":3,"name":"Biology"],
+        ["id":4,"name":"Business & Finance"],
+        ["id":5,"name":"Calculus"],
+        ["id":6,"name":"Chemistry"],
+        ["id":7,"name":"Economics"],
+        ["id":8,"name":"Engineering"],
+        ["id":9,"name":"English"],
+        ["id":10,"name":"Environmental Science"],
+        ["id":10,"name":"Foreign Languages"],
+        ["id":11,"name":"Geology"],
+        ["id":12,"name":"Health & Medical"],
+        ["id":13,"name":"History"],
+        ["id":14,"name":"Law"],
+        ["id":15,"name":"Marketing"],
+        ["id":16,"name":"Mathematics"],
+        ["id":17,"name":"Philosophy"],
+        ["id":18,"name":"Physics"],
+        ["id":19,"name":"Political Science"],
+        ["id":20,"name":"Computer Science"],
+        ["id":21,"name":"Psychology"],
+        ["id":22,"name":"Science"],
+        ["id":23,"name":"Social Science"],
+        ["id":24,"name":"Sociology"],
+        ["id":25,"name":"Statistics"],
+        ["id":26,"name":"Writing"],
+        ["id":27,"name":"Other"],
+        ["id":28,"name":"Algebra"],
+        ["id":29,"name":"Programming"],
+        ["id":30,"name":"Communications"],
+        ["id":31,"name":"Film"],
+        ["id":32,"name":"Management"],
+        ["id":33,"name":"SAT"]
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        println(data[0])
+        println(pickerData[0])
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -31,6 +66,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    //MARK: Delegates
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return pickerData[row]["name"]
+    }
+    
+  
 
 }
 

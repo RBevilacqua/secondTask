af26e657af55a557e1941ba17f73123c9b89dde2
JValderramaN <josevalderrama18@gmail.com>
2015-05-30 12:32:38 -0430

    updated search

--- secondtask/ViewController.swift
+++ secondtask/ViewController.swift
@@ -13,12 +13,14 @@ class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelega
     @IBOutlet weak var tfSearch: UITextField!
     @IBOutlet weak var btSearch: UIButton!
     @IBOutlet weak var picker: UIPickerView!
-    @IBOutlet weak var btPopular1: UIImageView!
-    @IBOutlet weak var btPopular2: UIImageView!
-    @IBOutlet weak var btPopular3: UIImageView!
-    @IBOutlet weak var btPopular4: UIImageView!
     
-    var pickerData = [
+    @IBOutlet weak var btPopular1: UIButton!
+    @IBOutlet weak var btPopular2: UIButton!
+    @IBOutlet weak var btPopular3: UIButton!
+    @IBOutlet weak var btPopular4: UIButton!
+    
+    
+    var pickerData : NSMutableArray = [
         ["id":1,"name":"Accounting"],
         ["id":2,"name":"Art & Design"],
         ["id":3,"name":"Biology"],
@@ -29,35 +31,35 @@ class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelega
         ["id":8,"name":"Engineering"],
         ["id":9,"name":"English"],
         ["id":10,"name":"Environmental Science"],
-        ["id":10,"name":"Foreign Languages"],
-        ["id":11,"name":"Geology"],
-        ["id":12,"name":"Health & Medical"],
-        ["id":13,"name":"History"],
-        ["id":14,"name":"Law"],
-        ["id":15,"name":"Marketing"],
-        ["id":16,"name":"Mathematics"],
-        ["id":17,"name":"Philosophy"],
-        ["id":18,"name":"Physics"],
-        ["id":19,"name":"Political Science"],
-        ["id":20,"name":"Computer Science"],
-        ["id":21,"name":"Psychology"],
-        ["id":22,"name":"Science"],
-        ["id":23,"name":"Social Science"],
-        ["id":24,"name":"Sociology"],
-        ["id":25,"name":"Statistics"],
-        ["id":26,"name":"Writing"],
-        ["id":27,"name":"Other"],
-        ["id":28,"name":"Algebra"],
-        ["id":29,"name":"Programming"],
-        ["id":30,"name":"Communications"],
-        ["id":31,"name":"Film"],
-        ["id":32,"name":"Management"],
-        ["id":33,"name":"SAT"]
+        ["id":11,"name":"Foreign Languages"],
+        ["id":12,"name":"Geology"],
+        ["id":13,"name":"Health & Medical"],
+        ["id":14,"name":"History"],
+        ["id":15,"name":"Law"],
+        ["id":16,"name":"Marketing"],
+        ["id":17,"name":"Mathematics"],
+        ["id":18,"name":"Philosophy"],
+        ["id":19,"name":"Physics"],
+        ["id":20,"name":"Political Science"],
+        ["id":21,"name":"Computer Science"],
+        ["id":22,"name":"Psychology"],
+        ["id":23,"name":"Science"],
+        ["id":24,"name":"Social Science"],
+        ["id":25,"name":"Sociology"],
+        ["id":26,"name":"Statistics"],
+        ["id":27,"name":"Writing"],
+        ["id":28,"name":"Other"],
+        ["id":29,"name":"Algebra"],
+        ["id":30,"name":"Programming"],
+        ["id":31,"name":"Communications"],
+        ["id":32,"name":"Film"],
+        ["id":33,"name":"Management"],
+        ["id":34,"name":"SAT"]
     ]
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        println(pickerData[0])
+     
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -69,18 +71,52 @@ class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelega
     
     
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        
         return 1
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        btPopular1.layer.cornerRadius = 38
+        btPopular2.layer.cornerRadius = 38
+        btPopular3.layer.cornerRadius = 38
+        btPopular4.layer.cornerRadius = 38
+        
+      
+        
+    }
+    
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return pickerData.count
     }
     
     //MARK: Delegates
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
-        return pickerData[row]["name"]
+        return pickerData[row]["name"] as! String
     }
     
+    //first index for ID, second for index of subject in array
+    func findSubject(subjectName : String) -> [Int]{
+        var response = [-1,-1]
+        for index  in 0...pickerData.count-1{
+            let subject: AnyObject = pickerData[index]
+            if (subject["name"] as! String).lowercaseString == subjectName.lowercaseString{
+                response = [subject["id"] as! Int,index]
+                return response
+            }
+        }
+        return response
+    }
   
+    @IBAction func searchButtonTapped(sender: UIButton) {
+        tfSearch.resignFirstResponder()
+        self.view.endEditing(true)
+        
+        let response = findSubject(tfSearch.text)
+        if response[0] != -1 {
+            picker.selectRow(response[1], inComponent: 0, animated: true)
+        }
+    }
 
 }
 
